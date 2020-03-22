4986a3eef7585c5c0dfd8916959b97e85f2a362c
Steve Bronder <stevo15025@gmail.com>
2020-03-22 19:39:53 -0400

    add pushback

--- resize_vs_reserve.cpp
+++ resize_vs_reserve.cpp
@@ -29,8 +29,21 @@ static void reserve_bench(benchmark::State& state) {
   }
 }
 
+static void reserve_pushback_bench(benchmark::State& state) {
+  for (auto _ : state) {
+    std::vector<double> a(0);
+    a.reserve(state.range(0));
+    benchmark::DoNotOptimize(a.data());
+    for (int i = 0; i < state.range(0); i++) {
+      a.push_back(10.0);
+    }
+    benchmark::ClobberMemory();
+  }
+}
+
 // The start and ending sizes for the benchmark
 BENCHMARK(resize_bench)->DenseRange(10, 2010, 1000);
 BENCHMARK(reserve_bench)->DenseRange(10, 2010, 1000);
+BENCHMARK(reserve_pushback_bench)->DenseRange(10, 2010, 1000);
 
 BENCHMARK_MAIN();
