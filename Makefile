15bbbafc1070aed7745de169e6f79e7405866f7a
Steve Bronder <stevo15025@gmail.com>
2020-03-22 16:58:46 -0400

    add resize_vs_reserve

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ MATH ?=math/
 include math/make/compiler_flags
 include math/make/dependencies                 # rules for generating dependencies
 include math/make/libraries
-CXXFLAGS+=-O3 -march=native -mtune=native -Ibenchmark/include -std=c++1y -Imath/ -I$(BOOST) -I$(SUNDIALS)/include -I$(EIGEN)
+CXXFLAGS+=-O3  -march=native -mtune=native -Ibenchmark/include -std=c++1y -Imath/ -I$(BOOST) -I$(SUNDIALS)/include -I$(EIGEN)
 LDLIBS+=-lbenchmark
 LDFLAGS+=-Lbenchmark/build/src
 CXX ?= clang++
---
+++ resize_vs_reserve.cpp
@@ -0,0 +1,36 @@
+
+#include <benchmark/benchmark.h>
+#include <utility>
+#include <chrono>
+
+
+static void resize_bench(benchmark::State& state) {
+
+  for (auto _ : state) {
+    std::vector<double> a(0);
+    a.resize(state.range(0));
+    benchmark::DoNotOptimize(a.data());
+    for (int i = 0; i < a.size(); i++) {
+      a[i] = 10.0;
+    }
+    benchmark::ClobberMemory();
+  }
+}
+
+static void reserve_bench(benchmark::State& state) {
+  for (auto _ : state) {
+    std::vector<double> a(0);
+    a.reserve(state.range(0));
+    benchmark::DoNotOptimize(a.data());
+    for (int i = 0; i < state.range(0); i++) {
+      a[i] = 10.0;
+    }
+    benchmark::ClobberMemory();
+  }
+}
+
+// The start and ending sizes for the benchmark
+BENCHMARK(resize_bench)->DenseRange(10, 2010, 1000);
+BENCHMARK(reserve_bench)->DenseRange(10, 2010, 1000);
+
+BENCHMARK_MAIN();
